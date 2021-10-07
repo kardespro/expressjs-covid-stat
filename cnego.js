d80fc3f29b0590ec3a41f20b8c37083dec91f887
kardespro <61694826+kardespro@users.noreply.github.com>
2021-10-08 01:29:31 +0400

    Update cnego.js

--- cnego.js
+++ cnego.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express()
+const axi = require("axios");
 
 app.set('view engine', 'ejs')
 
 app.get("/", (req,res) => {
 res.render("home");
 });
-
+app.post("/api", async(req,res) => {
+var b = req.body.nego;
+var resnego = await axi.get(`https://disease.sh/v3/covid-19/countries/${b}`)
+return res.json({
+ölüm: resnego.deaths,
+aktiv: resnego.active,
+recovered: resnego.recovered,
+activePerOneMillion: resnego.activePerOneMillion
+});
+});
 
 console.log(`Hazır`);
 app.listen(6060)
